f1a48d21f8bf24c4af9ef3e4ae6ea23e1e1df508
Sascha Adler <sascha.adler@canadiana.ca>
2017-08-23 15:21:33 -0400

    Initial commit. Provides generic JWT functionality. Services which require checking for additional credentials can do so in subsequent middleware

---
+++ __tests__/index.js
@@ -0,0 +1,65 @@
+const Koa = require('koa');
+const http = require('http');
+const request = require('supertest');
+const nJwt = require('njwt');
+
+const secrets = require('./config').secrets;
+
+let jwts = {
+  full: nJwt.create({ iss: 1 }, secrets[1]),
+  withParam: nJwt.create({ iss: 1, param: 'foo' }, secrets[1]),
+  otherKey: nJwt.create({ iss: 1 }, secrets[2])
+};
+
+jwts.expired = nJwt.create({}, secrets[1]);
+jwts.expired.setExpiration(new Date('2017-01-01'));
+
+const jwt = require('..');
+
+const app = new Koa();
+app.context.config = { secrets };
+
+app.use(jwt());
+
+// let's assume for testing purposes that credentialed requests get 200
+app.use(ctx => { ctx.status = 200; });
+
+it('throws 401 when lacking credentials', () => {
+  return request(http.createServer(app.callback()))
+    .get('/foo')
+    .expect(401);
+});
+
+it('validates full credentials', () => {
+  return request(http.createServer(app.callback()))
+    .get('/foo')
+    .set('Authorization', `C7A2 ${jwts.full.compact()}`)
+    .expect(200);
+});
+
+it('validates full credentials using query string parameter', () => {
+  return request(http.createServer(app.callback()))
+    .get(`/foo?token=${jwts.full.compact()}`)
+    .expect(200);
+});
+
+it('validates full credentials using cookie', () => {
+  return request(http.createServer(app.callback()))
+    .get('/foo')
+    .set('Cookie', `c7a2_token=${jwts.full.compact()}`)
+    .expect(200);
+});
+
+it('rejects JWTs from incorrect issuers', () => {
+  return request(http.createServer(app.callback()))
+    .get('/foo')
+    .set('Authorization', `C7A2 ${jwts.otherKey.compact()}`)
+    .expect(401);
+});
+
+it('throws 401 when JWT is expired', () => {
+  return request(http.createServer(app.callback()))
+    .get('/foo')
+    .set('Authorization', `C7A2 ${jwts.expired.compact()}`)
+    .expect(401);
+});
---
+++ index.js
@@ -0,0 +1,48 @@
+const unless = require('koa-unless');
+const nJwt = require('njwt');
+
+module.exports = (opts) => {
+  const middleware = async function jwt(ctx, next) {
+    let token = ctx.query['token'] ||
+      ctx.cookies.get('c7a2_token') ||
+      ((ctx.headers.authorization || '').match(/^C7A2 (.+)$/i) || [])[1] ||
+      null;
+
+    if (!token) {
+      ctx.throw(401, `Access requires a signed JWT in the Authorization header, in the form
+
+Authorization: C7A2 $jwt
+
+Alternatively, the JWT can be provided in the query string
+
+?token=$token
+
+or be set in a cookie, with key c7a2_token`);
+    }
+
+    let validatedIssuer, jwtData;
+    for (issuer of Object.keys(ctx.config.secrets)) {
+      try {
+        jwtData = nJwt.verify(token, ctx.config.secrets[issuer]).body;
+        validatedIssuer = issuer;
+        break;
+      } catch (err) {
+      }
+    }
+
+    if (!jwtData) {
+      ctx.throw(401, 'Could not verify JWT');
+    }
+
+    if (jwtData.iss != validatedIssuer) {
+      ctx.throw(401, `Validated issuer ${validatedIssuer} does not match JWT 'iss' claim`);
+    }
+
+    ctx.state.cihmJwt = jwtData;
+
+    return next();
+  };
+
+  middleware.unless = unless;
+  return middleware;
+};
